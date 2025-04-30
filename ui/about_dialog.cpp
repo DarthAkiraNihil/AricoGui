638226a1d3b7f1359839bb341a2dc1da92dbb071
DarthAkiraNihil <egorzverev666@gmail.com>
2025-04-30 22:05:33 +0300

    Add about text and license

--- ui/about_dialog.cpp
+++ ui/about_dialog.cpp
@@ -9,6 +9,9 @@
 #include <QPushButton>
 
 namespace UI {
+    
+    inline const QString ABOUT = "Курсовой проект по дисциплине \"АСД\", представляющий собой реализацию целочисленного арифметического кодера и декодера.\n\nАвтор - Егор \"Akira Nihil\" Зверев - https://github.com/DarthAkiraNihil\n\nАлгоритм разработан с применением Python 3.12.9\n\nИнтерфейс разработан с применением C++26 и Qt 6.8.0\n\nПроект лицензирован под Apache 2.0. License";
+    
     AboutDialog::AboutDialog(QWidget *parent) :
         QDialog(parent), ui(new Ui::AboutDialog) {
         this->ui->setupUi(this);
@@ -24,9 +27,11 @@ namespace UI {
         this->ui->labelLogo->setPixmap(QPixmap(":/assets/logo.png"));
         this->ui->labelLogo->setScaledContents(true);
         
+        this->ui->labelInfo->setText(ABOUT);
+        
     }
     
     AboutDialog::~AboutDialog() {
-        delete ui;
+        delete this->ui;
     }
 } // UI
